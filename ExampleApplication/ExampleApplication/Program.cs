7af0f2155b268e8ad9c2389752f1b6d097d69786
piyushchouhan <piyush1.befoodly@gmail.com>
2023-04-03 17:05:06 +0530

    Http Request Header

--- ExampleApplication/ExampleApplication/Program.cs
+++ ExampleApplication/ExampleApplication/Program.cs
@@ -9,9 +9,16 @@ var app = builder.Build(); // As a result of build method you will get an instan
 
 app.Run(async (HttpContext context) => // Run function is used to handle request
 {
-    context.Response.StatusCode = 400;
-    await context.Response.WriteAsync("Hello");
-    await context.Response.WriteAsync(" World");
+    if(context.Request.Method == "GET") {
+        if (context.Request.Query.ContainsKey("id")) {
+            string id = context.Request.Query["id"];
+            await context.Response.WriteAsync($"<p>{id}</p>");
+        }
+    }
+    //context.Response.StatusCode = 400;
+    //context.Response.Headers["MyKey"] = "Value";
+    //await context.Response.WriteAsync("Hello");
+    //await context.Response.WriteAsync(" World");
 });
 
 app.Run(); // Call the run method to start the server.
